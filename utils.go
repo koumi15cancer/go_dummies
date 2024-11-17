6d31ac3a4afaf03ad21581128eabf06928a0b068
koumi15cancer <daiduong040700@gmail.com>
2024-11-17 14:57:21 +0700

    (+) utils to return token auth and encode json payload

---
+++ utils.go
@@ -0,0 +1,30 @@
+package main
+import {
+	"encoding/json"
+	"net/http"
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(v)
+}
+
+func GetTokeFromRequest(r *http.Request) string{
+	tokenAuth := r.Header.get("Authorization")
+	tokenQuery := r.URL.Query().Get("token")
+
+	if tokenAuth != "" {
+		return tokenAuth
+	}
+
+	if tokenQuery != "" {
+		return tokenQuery
+	}
+
+	return ""
+}
